987291a495faaccfcddad806c9bc605532560407
MinoUni <maks.reva.ua@gmail.com>
2024-04-25 21:02:56 +0300

    Add User entity

---
+++ src/main/java/com/example/test/user/User.java
@@ -0,0 +1,46 @@
+package com.example.test.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "id", nullable = false)
+  private Integer id;
+
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
+
+  @Column(name = "birth_day")
+  private LocalDate birthDay;
+
+  private String address;
+
+  @Column(name = "phone_number")
+  private String phoneNumber;
+}
