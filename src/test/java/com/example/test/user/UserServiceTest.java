bbd343722aaed08734c477069470d6c25f8318f3
MinoUni <maks.reva.ua@gmail.com>
2024-04-26 12:50:35 +0300

    Refactor user full update tests in UserServiceTest.java

--- src/test/java/com/example/test/user/UserServiceTest.java
+++ src/test/java/com/example/test/user/UserServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.test.user;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
@@ -89,8 +92,8 @@ class UserServiceTest {
   }
 
   @Test
-  @DisplayName("when full update user with all details then return 200 status")
-  void whenFullFullUpdateUserWithAllDetailsProvidedThenResponseWithStatusCode200() {
+  @DisplayName("when full update user with all details then update successfully")
+  void whenFullUpdateUserWithAllDetailsProvidedThenUpdateSuccessfully() {
     final int userId = 1;
     User user = User.builder().id(userId).build();
     var details =
@@ -116,6 +119,58 @@ class UserServiceTest {
 
     verify(userRepository, times(1)).findById(eq(userId));
     verify(userRepository, times(1)).save(eq(user));
+  }
+
+  @Test
+  @DisplayName("when full update non-existing user then throw UserNotFoundException")
+  void whenFullUpdateNotExistingUserWithAllDetailsThenThrowUserNotFoundException() {
+    final int userId = 1;
+    final String exceptionMessage = String.format("User with id <%d> not found", userId);
+    var details =
+        new UserFullUpdate(
+            "mark.jovar@gmail.com",
+            "Mark",
+            "Jovar",
+            LocalDate.of(2000, 3, 10),
+            "Adress",
+            "phone number");
+
+    when(userRepository.findById(eq(userId))).thenReturn(Optional.empty());
+    when(userRepository.save(any(User.class))).thenReturn(any(User.class));
+
+    var errorMessage =
+        assertThrows(UserNotFoundException.class, () -> userService.fullUpdate(userId, details));
+
+    assertEquals(exceptionMessage, errorMessage.getMessage());
+
+    verify(userRepository, times(1)).findById(eq(userId));
+    verify(userRepository, never()).save(any(User.class));
+  }
+
+  @Test
+  @DisplayName(
+      "when full update user with age less than age constraint then throw InvalidUserAgeException")
+  void whenFullUpdateUserWithAgeLessThanAgeConstraintThenThrowInvalidUserAgeException() {
+    final int userId = 1;
+    final String exceptionMessage = String.format("User age less than %d", AGE_CONSTRAINT);
+    User user = User.builder().firstName("Dummy").lastName("Dumbster").build();
+    var details =
+        new UserFullUpdate(
+            "mark.jovar@gmail.com", "Mark", "Jovar", LocalDate.now(), "Adress", "phone number");
+
+    when(userRepository.findById(eq(userId))).thenReturn(Optional.of(user));
+    when(userRepository.save(eq(user))).thenReturn(eq(user));
+
+    var errorMessage =
+        assertThrows(InvalidUserAgeException.class, () -> userService.fullUpdate(userId, details));
+
+    assertEquals(exceptionMessage, errorMessage.getMessage());
+
+    assertNotEquals(user.getFirstName(), details.firstName());
+    assertNotEquals(user.getLastName(), details.lastName());
+
+    verify(userRepository, never()).findById(eq(userId));
+    verify(userRepository, never()).save(any(User.class));
   }
 
   @Test
